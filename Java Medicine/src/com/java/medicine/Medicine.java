cf57508a374cf2343d4bf56bf325c318ad303ad5
ALAQSA <ALAQSA@DESKTOP-7L1U5NV>
2018-09-06 09:26:01 +0200

    If Statement

--- Java Medicine/src/com/java/medicine/Medicine.java
+++ Java Medicine/src/com/java/medicine/Medicine.java
@@ -18,39 +18,37 @@ public class Medicine {
 		String subject = input.nextLine();
 		System.out.println("Your Subject is : " + subject);
 		input.close();
-		
 
-		if (subject.equals("Physiology") ) {
+		if (subject.equals("Physiology")) {
 			System.out.println("Not Avaliable");
-			
-		} else if (subject.equals("Anatomy")){
+
+		} else if (subject.equals("Anatomy")) {
 			System.out.println("Avaliable in 9/8/2018");
-			
-		} else if (subject.equals("Bio")){
+
+		} else if (subject.equals("Bio")) {
 			System.out.println("Avaliable in next summer");
 		}
-			
-		if (age>=20 && age<26)  {
-				System.out.println(" Avaliable age");
-				
-			} else if (age>=26 && age >31){
-				System.out.println("Avaliable in 9/8/2018");
-				
-			} else {
-				System.out.println("Not Avaliable Age ");
-	}
+
+		if (age >= 20 && age < 26) {
+			System.out.println(" Avaliable age");
+
+		} else if (age >= 26 && age > 31) {
+			System.out.println("Avaliable in 9/8/2018");
+
+		} else {
+			System.out.println("Not Avaliable Age ");
+		}
 	}
 }
 
-	/*
-	 * public static void main(String[] args) { Scanner input = new
-	 * Scanner(System.in); System.out.print("Enter your name :  " ); String name =
-	 * input.nextLine();
-	 * 
-	 * System.out.print("your name :  " +name);
-	 * 
-	 * input.close();
-	 * 
-	 * }
-	 */
-
+/*
+ * public static void main(String[] args) { Scanner input = new
+ * Scanner(System.in); System.out.print("Enter your name :  " ); String name =
+ * input.nextLine();
+ * 
+ * System.out.print("your name :  " +name);
+ * 
+ * input.close();
+ * 
+ * }
+ */
