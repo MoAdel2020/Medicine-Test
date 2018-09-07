e04ceec2f64664421ae7edeec2d2465eab6b4bca
ALAQSA <ALAQSA@DESKTOP-7L1U5NV>
2018-09-07 03:27:25 +0200

    your

--- Java Medicine/src/com/java/medicine/Medicine2.java
+++ Java Medicine/src/com/java/medicine/Medicine2.java
@@ -3,33 +3,33 @@ package com.java.medicine;
 import java.util.Scanner;
 
 public class Medicine2 {
+	static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-
-		enterYouName(input);
-		int age = enterYouAge(input);
-		String subject = enterYouSubject(input);
-		
-		checkSubjects(input, subject, age);
-
+		enterYouName();
+		int age = enterYouAge();
+		String subject = enterYouSubject();
+		checkSubjects(subject, age);
 		input.close();
 
 	}
 
-	private static void enterYouName(Scanner input) {
+	private static void enterYouName() {
+		
 		System.out.print("Enter your name : ");
-		String name = input.nextLine();
+		String name= input.nextLine();
 		System.out.println("Your name is : " + name);
+
 	}
 
-	private static String enterYouSubject(Scanner input) {
+	private static String enterYouSubject() {
 		System.out.print("Subject : ");
 		String subject = input.nextLine();
 		System.out.println("Your Subject is : " + subject);
 		return subject;
 	}
 
-	private static void checkSubjects(Scanner input, String subject, int age) {
+	private static void checkSubjects( String subject, int age) {
 		if (subject.equals("Physiology")) {
 			System.out.println("Not Avaliable");
 
@@ -48,7 +48,7 @@ public class Medicine2 {
 		}
 	}
 
-	private static int enterYouAge(Scanner input) {
+	private static int enterYouAge() {
 		System.out.print("Enter your age : ");
 		int age = input.nextInt();
 		input.nextLine();
